dcd81c6ea49c23a8f4bff91ecaf50776b05745d1
arioum <amoghrao121@gmail.com>
2024-02-20 01:45:59 +0530

    added media queries

--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -1,26 +1,40 @@
-import Button from '../../components/Button/Button'
-import leadLogo from '../../assets/brand-logo.svg'
-import classes from './navbar.module.scss'
+import Button from '../../components/Button/Button';
+import leadLogo from '../../assets/brand-logo.svg';
+import classes from './navbar.module.scss';
 
 const Navbar = () => {
   return (
-    <nav className={classes.navbar}>
-      <div className="navbar__logo">
-        <img src={leadLogo} alt="logo" width={200} />
+    <nav className={`container ${classes.navbar}`}>
+      <div>
+        <img src={leadLogo} alt='logo' className={classes.navbar__logo} />
       </div>
       <div className={classes.navbar__navLinkContainer}>
-        <li className={classes.navbar__navLinkContainer__navItems}>About Us</li>
-        <li className={classes.navbar__navLinkContainer__navItems}>Team</li>
-        <li className={classes.navbar__navLinkContainer__navItems}>Portfolio</li>
-        <li className={classes.navbar__navLinkContainer__navItems}>Pricing</li>
-        <li className={classes.navbar__navLinkContainer__navItems}>Help</li>
+        <a href='/' className={classes.navbar__navLinkContainer__navItems}>
+          About Us
+        </a>
+        <a href='/' className={classes.navbar__navLinkContainer__navItems}>
+          Team
+        </a>
+        <a href='/' className={classes.navbar__navLinkContainer__navItems}>
+          Portfolio
+        </a>
+        <a href='/' className={classes.navbar__navLinkContainer__navItems}>
+          Pricing
+        </a>
+        <a href='/' className={classes.navbar__navLinkContainer__navItems}>
+          Help
+        </a>
       </div>
       <div className={classes.navbar__btnContainer}>
-        <Button variant='secondary' size='small'>Log in</Button>
-        <Button variant='primary' size='small'>Sign Up</Button>
+        <Button variant='secondary' size='small'>
+          Log in
+        </Button>
+        <Button variant='primary' size='small'>
+          Sign Up
+        </Button>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
